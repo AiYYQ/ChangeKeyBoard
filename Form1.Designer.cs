360b52edf70ee9d0eaee6b0ff64e2ac90174e9f6
AIYYQ <yq5202016@outlook.com>
2021-02-01 22:52:45 +0800

    修改bug

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,12 @@ namespace 修改键盘重复延迟
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(87, 45);
+            this.button1.Location = new System.Drawing.Point(88, 111);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(184, 23);
             this.button1.TabIndex = 0;
@@ -42,21 +43,35 @@ namespace 修改键盘重复延迟
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(368, 187);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 
 namespace 修改键盘重复延迟
-
-    
 {
     public partial class Form1 : Form
     {
@@ -22,7 +21,52 @@ namespace 修改键盘重复延迟
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("此时重复延迟为" + 2);
+            int keyBoardDelay = SystemInformation.KeyboardDelay;
+            int keyBoardSpeed = SystemInformation.KeyboardSpeed;
+
+            StringBuilder sbuilder = new StringBuilder();
+            sbuilder.Append("当前键盘重复延迟为");
+            sbuilder.Append(keyBoardDelay);
+            sbuilder.AppendLine();
+            sbuilder.Append("当前键盘重复速度为");
+            sbuilder.Append(keyBoardSpeed);
+            MessageBox.Show(sbuilder.ToString(),"属性");
         }
+
+        
+        protected void changeRegistry()
+        {
+            RegistryKey registryKey = Registry.CurrentUser;
+            RegistryKey target = registryKey.OpenSubKey(@"Control Panel\Keyboard\", true);
+            if (target == null)
+            {
+                this.label1.Text = "查询注册表失败,注册表中不存在该项";
+                return;
+            }
+            // 获取注册表源值
+            string a = target.GetValue("KeyboardSpeed").ToString();
+            if ("48".Equals(a))
+            {
+                this.label1.Text = "修复成功";
+                return;
+            }
+            else
+            {
+                this.label1.Text = "当前键盘重复速度为" + a;
+            }
+            // 修改注册表
+            target.SetValue("KeyboardSpeed", "48");
+            a = target.GetValue("KeyboardSpeed").ToString();
+            this.label1.Text = "当前键盘重复速度为" + a + "修改成功!";
+            registryKey.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            changeRegistry();
+        }
+
+        
     }
+
 }
